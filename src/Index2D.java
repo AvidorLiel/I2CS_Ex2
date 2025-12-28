3e81f68b5c86df225675e2ba51af003a3f53cde2
alex avidor <alexav@pelephone.co.il>
2025-12-28 20:37:05 +0200

    all the project

--- src/Index2D.java
+++ src/Index2D.java
@@ -69,44 +69,5 @@ public class Index2D implements Pixel2D
         return ans;
     }
 
-    /**
-     * Intro2CS_2026A
-     * This class represents a Graphical User Interface (GUI) for Map2D.
-     * The class has save and load functions, and a GUI draw function.
-     * You should implement this class, it is recommender to use the StdDraw class, as in:
-     * https://introcs.cs.princeton.edu/java/stdlib/javadoc/StdDraw.html
-     *
-     *
-     */
-    public static class Ex2_GUI {
-        public static void drawMap(Map2D map) {
-            //
-        }
 
-        /**
-         * @param mapFileName
-         * @return
-         */
-        public static Map2D loadMap(String mapFileName) {
-            Map2D ans = null;
-
-            return ans;
-        }
-
-        /**
-         *
-         * @param map
-         * @param mapFileName
-         */
-        public static void saveMap(Map2D map, String mapFileName) {
-
-
-        }
-        public static void main(String[] a) {
-            String mapFile = "map.txt";
-            Map2D map = loadMap(mapFile);
-            drawMap(map);
-        }
-        /// ///////////// Private functions ///////////////
-    }
 }
